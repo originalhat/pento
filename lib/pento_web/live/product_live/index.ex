b724e2aece69da17f086a089449b01dc32b9596e
Devin Brown <devin.g.brown@gmail.com>
2021-06-01 08:04:24 -0600

    <<<<<<<<<<<<<<< HEAD

--- lib/pento_web/live/product_live/index.ex
+++ lib/pento_web/live/product_live/index.ex
@@ -6,7 +6,7 @@ defmodule PentoWeb.ProductLive.Index do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, :products, list_products())}
+    {:ok, socket |> assign(:greeting, "Welcome to Pento!") |> assign(:products, list_products())}
   end
 
   @impl true
