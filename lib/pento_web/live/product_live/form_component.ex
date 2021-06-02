d0a4645f167b718fe0e04923192ef3c0226c2888
Devin Brown <devin.g.brown@gmail.com>
2021-06-02 16:04:39 -0600

    product images (at least the default); p151

--- lib/pento_web/live/product_live/form_component.ex
+++ lib/pento_web/live/product_live/form_component.ex
@@ -10,7 +10,16 @@ defmodule PentoWeb.ProductLive.FormComponent do
     {:ok,
      socket
      |> assign(assigns)
-     |> assign(:changeset, changeset)}
+     |> assign(:changeset, changeset)
+     |> allow_upload(:image,
+       accept: ~w(.jpg .jpeg .png),
+       max_entries: 1,
+       progress: &handle_progress/3
+     )}
+  end
+
+  def update_changeset(%{assigns: %{changeset: changeset}} = socket, key, value) do
+    socket |> assign(:changeset, Ecto.Changeset.put_change(changeset, key, value))
   end
 
   @impl true
@@ -25,6 +34,28 @@ defmodule PentoWeb.ProductLive.FormComponent do
 
   def handle_event("save", %{"product" => product_params}, socket) do
     save_product(socket, socket.assigns.action, product_params)
+  end
+
+  defp handle_progress(:image, entry, socket) do
+    if entry.done? do
+      path = consume_uploaded_entry(socket, entry, &upload_static_file(&1, socket))
+
+      IO.inspect "################"
+      IO.inspect path
+
+      {:noreply,
+       socket
+       |> put_flash(:info, "file #{entry.client_name} uploaded")
+       |> update_changeset(:image_upload, path)}
+    else
+      {:noreply, socket}
+    end
+  end
+
+  defp upload_static_file(%{path: path}, socket) do
+    dest = Path.join("priv/static/images", Path.basename(path))
+    File.cp!(path, dest)
+    Routes.static_path(socket, "/images/#{Path.basename(dest)}")
   end
 
   defp save_product(socket, :edit, product_params) do
