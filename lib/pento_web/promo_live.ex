08f86b838fc6ba5c9b23d65cd5d263310e238b37
Devin Brown <devin.g.brown@gmail.com>
2021-06-02 08:19:18 -0600

    promo form; p139

--- lib/pento_web/promo_live.ex
+++ lib/pento_web/promo_live.ex
@@ -5,9 +5,36 @@ defmodule PentoWeb.PromoLive do
   alias Pento.Promo.Recipient
 
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok, socket |> assign_recipient() |> assign_changeset()}
   end
 
+  def assign_recipient(socket) do
+    socket |> assign(:recipient, %Recipient{})
+  end
+
+  def assign_changeset(%{assigns: %{recipient: recipient}} = socket) do
+    socket |> assign(:changeset, Promo.change_recipient(recipient))
+  end
+
+  def handle_event(
+        "validate",
+        %{"recipient" => recipient_params},
+        %{assigns: %{recipient: recipient}} = socket
+      ) do
+    changeset =
+      recipient |> Promo.change_recipient(recipient_params) |> Map.put(:action, :validate)
+
+    {:noreply, socket |> assign(:changeset, changeset)}
+  end
+
+  def handle_event("save", %{"recipient" => recipient_params}, socket) do
+    case Promo.send_promo(socket.assigns.recipient, recipient_params) do
+      {:ok, _recipient} ->
+        {:noreply,
+         socket
+         |> put_flash(:info, "Promo sent successfully")}
+    end
+  end
   def render(assigns) do
     ~L"""
       <h1>Send Your Promo Code to a Friend</h2>
@@ -15,6 +42,21 @@ defmodule PentoWeb.PromoLive do
       <h4>
         Enter your friend's promo email below and we'll send them a promo code for 10% off their first game purchase!
       </h4>
+
+      <%= f = form_for @changeset, "#",
+        id: "promo-form",
+        phx_change: "validate",
+        phx_submit: "save" %>
+
+        <%= label f, :first_name %>
+        <%= text_input f, :first_name, phx_debounce: "blur" %>
+        <%= error_tag f, :first_name %>
+
+        <%= label f, :email %>
+        <%= text_input f, :email %>
+        <%= error_tag f, :email %>
+
+        <%= submit "Send promo", phx_disable_with: "Send promo..." %>
     """
   end
 end
